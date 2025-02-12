374b7c5765808de197539f3477183496391afc91
Aroldo68 <k21ajo@gmail.com>
2025-02-12 16:56:02 -0300

    Soluções

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,4 +1,17 @@
+import Videos from "../../json/videos.json";
 import styles from "./Caterory.module.css";
+
+export const categories = [
+    "Geografia",
+    "Como fazer e usar",
+    "Astronomia e Geografia",
+    "Climatologia, Meteorologia, Vegetação",
+    "Geologia e Hidrografia"
+  ]
+  
+export function filterCategory(id) {
+    return Videos.filter( video => video.category === categories[id] )
+}
 
 function Category({ category, children }) {
     return (
