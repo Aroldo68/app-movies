99aa8b9cd2142d00a0d58c37cc644ccdcd8fe9d7
Aroldo68 <k21ajo@gmail.com>
2025-02-17 16:50:06 -0300

    Criação das paginas no site

---
+++ src/pages/Watch/index.js
@@ -0,0 +1,12 @@
+import styles from "./Watch.module.css";
+
+function Watch() {
+    return (
+        <section className={StyleSheet.watch}>
+            <h1>Assistir</h1>
+        </section>
+        
+    )
+}
+
+export default Watch;
