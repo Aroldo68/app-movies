7d78010fd79db59c45aa2d877a3b9cda59afc79f
Aroldo68 <k21ajo@gmail.com>
2025-03-28 16:52:31 -0300

    Sistema de busca

---
+++ src/components/SearchVideoList/index.js
@@ -0,0 +1,11 @@
+import styles from "./SearchVideoList.module.css";
+
+function SearchVideoList() {
+    return (
+        <section className={styles.container}>
+
+        </section>
+    );
+}
+
+export default SearchVideoList;
