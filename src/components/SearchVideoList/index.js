bb546bd62d6bfda77846e93dbb03cf55697575fd
Aroldo68 <k21ajo@gmail.com>
2025-04-02 17:48:21 -0300

    Filtro de videos

--- src/components/SearchVideoList/index.js
+++ src/components/SearchVideoList/index.js
@@ -1,9 +1,20 @@
 import styles from "./SearchVideoList.module.css";
+import VideoList from "../../components/VideoList";
+import { use, useState } from "react";
 
-function SearchVideoList() {
+function SearchVideoList({ videos }) {
+
+    const [ searchText, setSearchText ] = useState()
+    const foundVideos = filterVideos(videos, searchText)
+
     return (
         <section className={styles.container}>
-
+            <input 
+                type="search"
+                placeholder="Pesquisar...."
+                onChange={}
+            />
+            <VideoList videos={videos} />
         </section>
     );
 }
